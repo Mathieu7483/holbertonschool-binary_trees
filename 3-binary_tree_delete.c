a6726a8c503853952aee4abee373013ef2052f82
Mathieu7483 <11436@holbertonstudents.com>
2025-08-04 13:19:56 +0200

    fixing code

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,9 +7,10 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 	return;
-
-	binary_tree_deleteb(tree->left);
-	binary_tree_deleteb(tree->right);
+	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
