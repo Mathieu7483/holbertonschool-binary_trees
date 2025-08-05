e64f3bcec0dea68014e4266c1326d73f6a51e50b
Mathieu7483 <11436@holbertonstudents.com>
2025-08-05 10:46:27 +0200

    fix: correct documentation for binary_tree_depth function to reflect depth measurement

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
-*binary_tree_depth - a function that measures the height of a binary tree
-*@tree:  is a pointer to the root node of the tree to measure the height.
-*Return: Height
+*binary_tree_depth - a function that measures the depth of a binary tree
+*@tree:  is a pointer to the root node of the tree to measure the depth.
+*Return: depth
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
