0a9143bfa97a1145a185be50ed5fb4d9586a4415
Mathieu7483 <11436@holbertonstudents.com>
2025-08-05 08:47:33 +0200

    add binary tree nodes counting function

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+*binary_tree_nodes -  function that counts the leaves in a binary tree
+*@tree: is a pointer to the root node of the tree to count the number of leaves
+*Return: Height
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+	return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	return (1 + binary_tree_nodes(tree->left) + (binary_tree_nodes(tree->right)));
+}
